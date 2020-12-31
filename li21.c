de8de42d1385f8e6b89e3b95e80bcbb3dce2cdb6
lilili-dev <74320300+lilili-dev@users.noreply.github.com>
2020-12-31 16:03:32 +0800

    l

--- li21.c
+++ li21.c
@@ -8,21 +8,27 @@ struct ListNode {
 
 static struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2)
 {
-    if(l1==NULL){
-        return l2;
-    }
-    if(l2==NULL){
-        return l1;
-    }
-   if(l1->val<=l2->val){
-       l1->next = mergeTwoLists(l1->next, l2);
-       return l1;
-   }else{
-       l2->next = mergeTwoLists(l1, l2->next);
-       return l2;
-   }
+    struct ListNode dummy;
+    struct ListNode *prev = &dummy;//指针变量
  
-    
+    while (l1 != NULL && l2 != NULL) {
+        if (l1->val <= l2->val) {
+           prev->next = l1;
+            l1 = l1->next;
+        } else {
+            prev->next = l2;
+            l2 = l2->next;
+        }
+        prev = prev->next;
+    }
+
+    if (l2 != NULL) {
+        prev->next = l2;
+    }else{
+       prev->next = l1;
+    }
+
+    return dummy.next;
 }
 
 int main(int argc, char **argv)
